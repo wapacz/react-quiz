16313a53db932f591aa02421fdd7b89d7d9eabc4
wapacz <michal.lapacz@gmail.com>
2021-04-02 00:55:16 +0200

    module5

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,11 +10,18 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
 
+    showNextQuestion() {
+        this.setState({
+            quiz_position: this.state.quiz_position + 1
+        })
+    }
+
     render() {
         const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
         return (
             <div>
-                {!isQuizEnd && <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
+                {!isQuizEnd && <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)}
+                                             quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>}
                 {!!isQuizEnd && <QuizEnd/>}
             </div>
         )
